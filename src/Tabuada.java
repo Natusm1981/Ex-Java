71734cdcd86ae4bb8eca54845a1e7f0c24599914
Nataniel <natuss.m@~gmail.com>
2023-01-18 14:34:43 -0300

    Exercicio tabuada

---
+++ src/Tabuada.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite um número para ver sua tabuada de 1 a 10: ");
+        int numero = entrada.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+
+    }
+}
