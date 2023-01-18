6699ae27dfff523940bf387cae747120888df2ff
Nataniel <natuss.m@~gmail.com>
2023-01-18 14:55:32 -0300

    correção exercicio tabuada

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -11,6 +11,7 @@ public class Tabuada {
         for(int i = 1; i <= 10; i++){
             System.out.println(numero + " x " + i + " = " + (numero * i));
         }
+        entrada.close();
 
     }
 }
